88b6a4c408336a0e1542853be0e8f91efc994603
Pyae Zin Phyo <u6715150@au.edu>
2026-02-26 11:15:12 +0700

    update cors for azure deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // --- 1. MIDDLEWARE ---
 // Must be placed BEFORE routes
-app.use(cors({ origin: "http://localhost:5173" })); 
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
